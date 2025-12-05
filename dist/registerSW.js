10a39b45ef7ffdbbb8bf486e1332c9baee98f3a1
Salar Khosravi <salar.khosravi@gmail.com>
2025-12-05 21:39:35 +0330

    fix vite config

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/frontend/dist/sw.js', { scope: '/frontend/dist/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
